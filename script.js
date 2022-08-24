a497e787006a49c242a2d530095de9b1f914d64a
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2022-08-24 14:26:09 +0300

    Preloader

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* Preloader */
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+}
+
 /* Smooth scroll */
 const smoothLinks = document.querySelectorAll("a[href^='#']");
 for (let smoothLink of smoothLinks) {
