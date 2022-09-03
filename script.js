849ba04a0ed8decc6bfd7449316486ca1ea44fa9
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2022-09-03 23:57:36 +0300

    Cart functional

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ for (let smoothLink of smoothLinks) {
         });
     });
 }
+
+/* Cart functional */
+
+const cartContainer = document.querySelector('.cart-content');
+const cartItems = cartContainer.children;
+
+const accessoriesForms = document.querySelectorAll('.accessories-form');
+
+const cartItemTemplate = document.querySelector('#cart-item-template').content;
+const newCartItemTemplate = cartItemTemplate.querySelector('.cart-item');
+
+for (let i = 0; i < accessoriesForms.length; i++) {
+	accessoriesForms[i].addEventListener('submit', function (evt) {
+		evt.preventDefault();
+
+		const accessoriesItemColor = accessoriesForms[i].querySelector('.radio-color:checked');
+		const accessoriesItemPrice = accessoriesForms[i].querySelector('.accessories-price');
+		const accessoriesItemName = accessoriesForms[i].querySelector('.accessories-submit');
+	
+		const itemColorContent = accessoriesItemColor.value;
+		const itemPriceContent = accessoriesItemPrice.textContent;
+		const itemNameContent = accessoriesItemName.value;
+		
+		let newCartItem = newCartItemTemplate.cloneNode(true);
+		let newCartItemName = newCartItem.querySelector('.cart-item-name');
+		let newCartItemColor = newCartItem.querySelector('.cart-item-color');
+		let newCartItemPrice = newCartItem.querySelector('.cart-item-price');
+
+		newCartItemName.textContent = itemNameContent;
+		newCartItemColor.textContent = itemColorContent;
+		newCartItemPrice.textContent = itemPriceContent;
+		cartContainer.appendChild(newCartItem);
+	});
+}
